92e19dbb7387fd3761d27519b94d5a7d3a0392c8
Nick <nsagias@gmail.com>
2023-03-24 17:10:23 -0400

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: "hsl(180, 66%, 49%)",
+        cyanLight: "hsl(180, 66%, 69%)",
+        darkViolet: "hsl(257, 27%, 26%)",
+        red: "hsl(0, 87%, 67%)",
+        grayViolet: "hsl(257, 7%, 63%)",
+        darkerBlue: "(255, 11%, 22%)",
+        darkerViolet: "hsl(260, 8%, 15%)"
+      }
+    },
   },
   plugins: [],
 }
