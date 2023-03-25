0aa7ac3e819859be41fadfea8e08dcb9d13c9b6b
Nick <nsagias@gmail.com>
2023-03-25 19:49:13 -0400

    add opacity to logo and update padding

--- src/components/navBar/NavLogo.tsx
+++ src/components/navBar/NavLogo.tsx
@@ -3,7 +3,7 @@ export default function NavLogo() {
     <>
     {/* todo add onClick/routing to home */}
       <button>
-        <img src="images/logo-tiny-app.svg" alt="" className="w-32 border-8 border-cyan px-4 py-1 rounded-full" />
+        <img src="images/logo-tiny-app.svg" alt="" className="w-32 border-8 border-cyan px-4 py-1 pt-2 rounded-full hover:opacity-70" />
       </button>
     </>
 
