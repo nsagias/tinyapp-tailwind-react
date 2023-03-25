8a08294dab3820f7d94770eee5af2f8203f8d7f3
Nick <nsagias@gmail.com>
2023-03-25 19:17:38 -0400

    add note

--- src/components/navBar/NavButtons.tsx
+++ src/components/navBar/NavButtons.tsx
@@ -1,6 +1,7 @@
 export default function NavButtons() {
   return (
     <>
+      {/* TODO add onclick method and routing */}
       <a href="#" className="hover:text-darkerViolet">Login</a>
       <a href="#" className="px-8 py-3 font-bold text-white bg-cyan rounded-full hover:opacity-70">Sign Up</a>
     </>
