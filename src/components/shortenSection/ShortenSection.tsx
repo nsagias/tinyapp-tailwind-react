7dd760adcb26a028bc90ce73ddba1cb87adb8a1a
Nick <nsagias@gmail.com>
2023-03-25 22:08:26 -0400

    add props to compoent

--- src/components/shortenSection/ShortenSection.tsx
+++ src/components/shortenSection/ShortenSection.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import { ShortenLinkData } from "../../types/ShortenLinks";
 import ShortenLink from "./ShortendLink";
 import ShortenForm from "./ShortenForm";
 import ShortenSectionContainer from "./ShortenSectionContainer";
 
+
+  
+const shorteLinkData: ShortenLinkData[] = [
+  { id: 1, fullLink: "https://somecoolwebsite.com" },
+];
+
 export default function ShortenSection() {
+
+  const [shortenLinks, setShortLinks] = useState<any>(shorteLinkData);
+
   return (
     <section id="shorten-section" className="relative bg-gray-100">
 
@@ -13,7 +24,7 @@ export default function ShortenSection() {
         <ShortenForm />
 
         {/* Display Shorten Links */}
-        <ShortenLink />
+        <ShortenLink data={shortenLinks}/>
 
       </ShortenSectionContainer>
 
--- src/components/shortenSection/ShortendLink.tsx
+++ src/components/shortenSection/ShortendLink.tsx
@@ -1,7 +1,16 @@
-export default function ShortenLink() {
+import { ShortenLinkData } from "../../types/ShortenLinks";
+
+export default function ShortenLink({ data }: { data: ShortenLinkData }): JSX.Element {
+
   return (
-    <div className="">
-      here
+    <div className="flex flex-col items-center justify-between w-full p-6 bg-white rounded-lg md:flex-row">
+      {/* Link name */}
+      <p className="font-bold text-center">{data.fullLink}</p>
+
+      {/* Shorten Link */}
+      <div className="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0">
+here
+      </div>
     </div>
   );
 }
