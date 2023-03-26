cdda654e0c0bb7f6d2655fcee2694b9f38f7877e
Nick <nsagias@gmail.com>
2023-03-26 16:30:57 -0400

    add modulo to button color and add null checking to data

--- src/components/shortenSection/ShortenLink.tsx
+++ src/components/shortenSection/ShortenLink.tsx
@@ -4,27 +4,26 @@ import CopyToClipboardButton from "../buttons/CopyToClipboardButton";
 
 export default function ShortenLink({ shortenLinks, index }: IShortenLinkProps ): JSX.Element {
 
-  const [copied, setCopied] = useState<string>(shortenLinks.shortLink);
-  console.log("INDEX", index)
+  const [copied, setCopied] = useState<string>(shortenLinks && shortenLinks.shortLink);
 
   return (
     <div className="flex flex-col items-center justify-between w-full p-6 bg-white rounded-lg md:flex-row">
 
       {/* Link name */}
-      <p className="font-bold text-center text-darkViolet md:text-left">{shortenLinks.fullLink}</p>
+      <p className="font-bold text-center text-darkViolet md:text-left">{shortenLinks && shortenLinks.fullLink}</p>
 
       {/* Shortened Link Container */}
       <div className="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0">
         
         {/* Display Short Link */}
         <div className="font-bold text-cyan">
-           {shortenLinks.shortLink}
+           {shortenLinks && shortenLinks.shortLink}
         </div>
 
         {/* Copy Button */}
         <CopyToClipboardButton 
           copy={copied} 
-          className={`p-2 px-8 text-white ${ true ? "bg-cyan" : "bg-darkViolet"} rounded-lg hover:opacity-70 focus:outline-none`}
+          className={`p-2 px-8 text-white ${ index && index % 2 ? "bg-darkViolet" : "bg-cyan" } rounded-lg hover:opacity-70 focus:outline-none`}
         />
     
       </div>
