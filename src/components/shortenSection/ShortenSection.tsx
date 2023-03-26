8e1fcaf64527ac9097d0ffa5d927e4ec6f42be7a
Nick <nsagias@gmail.com>
2023-03-26 03:57:33 -0400

    create shorten link and loop with links

--- src/components/shortenSection/ShortenSection.tsx
+++ src/components/shortenSection/ShortenSection.tsx
@@ -7,13 +7,13 @@ import ShortenSectionContainer from "./ShortenSectionContainer";
 
   
 const shorteLinkData: ShortenLinkData[] = [
-  { id: 1, fullLink: "https://somecoolwebsite.com" },
+  { id: 1, fullLink: "https://somecoolwebsite.com", shortLink: "https//tinyapp.ink/k4Ikyk" },
+  { id: 2, fullLink: "https://anothercoolwebsite.com", shortLink: "https//tinyapp.ink/AbCdefk" },
+  { id: 3, fullLink: "https://bestcoolwebsite.com", shortLink: "https//tinyapp.ink/Z24Ikyk" },
 ];
 
 export default function ShortenSection() {
-
   const [shortenLinks, setShortLinks] = useState<any>(shorteLinkData);
-
   return (
     <section id="shorten-section" className="relative bg-gray-100">
 
@@ -24,8 +24,9 @@ export default function ShortenSection() {
         <ShortenForm />
 
         {/* Display Shorten Links */}
-        <ShortenLink data={shortenLinks}/>
-
+        {shortenLinks && Array.isArray(shortenLinks) && shortenLinks.map((links) => (
+          <ShortenLink shortenLinks={links} key={links.id} />
+        ))}
       </ShortenSectionContainer>
 
     </section>
--- src/components/shortenSection/ShortendLink.tsx
+++ src/components/shortenSection/ShortendLink.tsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import { ShortenLinkData } from "../../types/ShortenLinks";
 
-export default function ShortenLink({ data }: { data: ShortenLinkData }): JSX.Element {
+export default function ShortenLink({ shortenLinks }: { shortenLinks: ShortenLinkData }): JSX.Element {
+
+  const [copied, setCopied] = useState<string>(shortenLinks.shortLink);
+
+  const handleCopy = async (e: any) => {
+ 
+      await navigator.clipboard.writeText(copied);
+      e.preventDefault();
+    
+  }
 
   return (
     <div className="flex flex-col items-center justify-between w-full p-6 bg-white rounded-lg md:flex-row">
-      {/* Link name */}
-      <p className="font-bold text-center">{data.fullLink}</p>
 
-      {/* Shorten Link */}
+      {/* Link name */}
+      <p className="font-bold text-center text-darkViolet md:text-left">{shortenLinks.fullLink}</p>
+
+      {/* Shortened Link Container */}
       <div className="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0">
-here
+        
+        {/* Short Link */}
+        <div className="font-bold text-cyan">
+           {shortenLinks.shortLink}
+        </div>
+
+        {/* Copy Button */}
+        <button  onClick={(e) => handleCopy(e)} className="p-2 px-8 text-white bg-cyan rounded-lg hover:opacity-70 focus:outline-none">Copy</button>
+
       </div>
     </div>
   );
