c1ed48cf4f079a3a90511b56b2573a5c24a6dfd1
Nick <nsagias@gmail.com>
2023-03-25 20:20:15 -0400

    create ShortenSection Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Hero from './components/heroSection/Hero';
 import NavBar from './components/navBar/NavBar';
+import ShortenSection from './components/shortenSection/ShortenSection';
 
 export default function App() {
   return (
     <div className="App">
       <NavBar />
       <Hero />
+      <ShortenSection />
     </div>
   );
 }
---
+++ src/components/shortenSection/ShortenSection.tsx
@@ -0,0 +1,6 @@
+export default function ShortenSection() {
+  return (
+    <section id="shorten-section" className="relative bg-gray-100">
+    </section>
+  );
+}
