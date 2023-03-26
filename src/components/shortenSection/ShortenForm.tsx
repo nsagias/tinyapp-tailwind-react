aea450e7e058d480a8a05d59f32bb0f7872461fa
Nick <nsagias@gmail.com>
2023-03-25 21:00:45 -0400

    add controlled component to input

--- src/components/shortenSection/ShortenForm.tsx
+++ src/components/shortenSection/ShortenForm.tsx
@@ -2,15 +2,28 @@ import { useState } from "react";
 
 export default function ShortenForm() {
 
-  const [shortenLink, setShortenLink] = useState<string>();
+  const [shortenLink, setShortenLink] = useState<string>("");
   
   const handleSubmitLink = (e: any) => {
     e.preventDefault();
   };
 
   return (
-    <form onSubmit={handleSubmitLink} id="link-form" className="relative flex flex-col w-full p-10 bg-darkViolet roundend-lg md:flex-row md:space-y-0 md:space-x-3">
-   
+    <form onSubmit={handleSubmitLink} 
+      id="link-form" 
+      className="relative flex flex-col w-full p-10 bg-darkViolet roundend-lg md:flex-row md:space-y-0 md:space-x-3">
+      <input
+        className="flex-1 p-3 border-2 rounded-lg placeholder-yellow-500 focus:outline-none"
+        id="shorten-form" 
+        name="shorten-link"
+        type={"text"}
+        placeholder={"Enter Link Here"}
+        value={shortenLink}
+        onChange={(e) => setShortenLink(e.target.value)}
+        required
+      >  
+      </input>
+      {/* <button>Submit</button> */}
     </form>
   );
 }
