3b32f1bfe9ffee60ce9b681e3439ce549867a2d0
Nick <nsagias@gmail.com>
2023-03-26 19:09:36 -0400

    add url validation

--- src/components/shortenSection/ShortenForm.tsx
+++ src/components/shortenSection/ShortenForm.tsx
@@ -1,12 +1,34 @@
 import { useState } from "react";
+import { validURL } from "../Utils/utils";
 
 export default function ShortenForm() {
 
   const [shortenLink, setShortenLink] = useState<string>("");
-  const [hasInputError, setHasInputError] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string | undefined>("");
   
-  const handleSubmitLink = (e: any) => {
+  const handleSubmitLink = async (e: any) => {
     e.preventDefault();
+    
+    console.log("FORM INPUT:", shortenLink);
+    // check for value
+    if (shortenLink === "") {
+      return setErrorMessage("Please Enter Value");
+    }
+ 
+    // check for valid link
+    const isValidURL = validURL(shortenLink);
+    if (!isValidURL) {
+      return setErrorMessage("Please Valid Link");
+    }
+   
+    // setvalue to clear
+    console.log("ISVALIDURL:",isValidURL);
+    // TODO: update database here
+    // TODO: refresh screen after database is updated
+    
+    // clear error message
+    setErrorMessage("");
+    
   };
 
   return (
@@ -14,14 +36,13 @@ export default function ShortenForm() {
       id="link-form" 
       className="relative flex flex-col w-full p-10 bg-darkViolet roundend-lg md:flex-row md:space-y-0 md:space-x-3">
       <input
-        className={`flex-1 p-3 border-2 rounded-lg placeholder-yellow-500 focus:outline-none ${hasInputError && "border-red border-8"}`}
+        className={`flex-1 p-3 border-2 rounded-lg placeholder-yellow-500 focus:outline-none ${errorMessage && "border-red border-8"}`}
         id="shorten-link" 
         name="shorten-link"
         type={"text"}
         placeholder={"Enter Link Here"}
         value={shortenLink}
         onChange={(e) => setShortenLink(e.target.value)}
-        required
         >  
       </input>
       <button
@@ -30,8 +51,8 @@ export default function ShortenForm() {
         >Shorten Link</button>
         
       {/* Error Message */}
-      { hasInputError && (
-        <div id="form-error-message" className="absolute left-7 bottom-3 text-red text-sm">Error</div>
+      { errorMessage && (
+        <div id="form-error-message" className="absolute lg:left-7 bottom-3 text-red text-sm">{errorMessage}</div>
       ) }
     </form>
   );
