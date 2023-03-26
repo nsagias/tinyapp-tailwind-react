27ecf3b4144c07b4e3f45d25a01beb99cd7c0039
Nick <nsagias@gmail.com>
2023-03-25 20:13:53 -0400

    add hero buttons container and heroImage container

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,5 +1,7 @@
+import HeroButtonsContainer from "./HeroButtonsContainer";
 import HeroContainer from "./HeroContainer";
 import HeroContentContainer from "./HeroContentContainer";
+import HeroImage from "./HeroImage";
 
 export default function Hero() {
   return (
@@ -17,13 +19,18 @@ export default function Hero() {
             <p className="text-2xl text-center text-gray-400 lg:max-w-md lg:text-left">Build your brand and get detailed insights on how your links are ranking.</p>
 
             {/* Button */}
-            <div className="mx-auto lg:mx-0">
-              <a href="# " className="py-5 px-10 text-2xl font-bold text-white bg-cyan rounded-full lg:py-4 hover:opacity-70">Get Started</a>
-            </div>
+            <HeroButtonsContainer>
 
+              {/* TODO ADD onClick and routing */}
+              <a href="# " className="py-5 px-10 text-2xl font-bold text-white bg-cyan rounded-full lg:py-4 hover:opacity-70">Get Started</a>
+            </HeroButtonsContainer>
+  
           </HeroContentContainer>
 
-          {/* Image Container */}
+          {/* Hero Image */}
+          <HeroImage />
+
+          
         </HeroContainer>
     </section>
   );
---
+++ src/components/hero/HeroButtonsContainer.tsx
@@ -0,0 +1,7 @@
+export default function HeroButtonsContainer({ children }: any): JSX.Element {
+  return (
+    <div className="mx-auto lg:mx-0">
+      {children}
+    </div>
+  );
+}
