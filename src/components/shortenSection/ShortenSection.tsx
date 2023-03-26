b65bfe5e5f498965ed1b845c336eac327e339d29
Nick <nsagias@gmail.com>
2023-03-26 17:13:39 -0400

    fix spelling

--- src/components/shortenSection/ShortenSection.tsx
+++ src/components/shortenSection/ShortenSection.tsx
@@ -4,14 +4,14 @@ import ShortenLink from "./ShortenLink";
 import ShortenForm from "./ShortenForm";
 import ShortenSectionContainer from "./ShortenSectionContainer";
   
-const shorteLinkData: ShortenLinkData[] = [
+const shortenLinkData: ShortenLinkData[] = [
   { id: 1, fullLink: "https://somecoolwebsite.com", shortLink: "https//tinyapp.ink/k4Ikyk" },
   { id: 2, fullLink: "https://anothercoolwebsite.com", shortLink: "https//tinyapp.ink/AbCdefk" },
   { id: 3, fullLink: "https://bestcoolwebsite.com", shortLink: "https//tinyapp.ink/Z24Ikyk" },
 ];
 
 export default function ShortenSection() {
-  const [shortenLinks, setShortLinks] = useState<any>(shorteLinkData);
+  const [shortenLinks, setShortLinks] = useState<any>(shortenLinkData);
   return (
     <section id="shorten-section" className="relative bg-gray-100">
 
