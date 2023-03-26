c759f1c481cbd0307679149be4fb66367f82e31e
Nick <nsagias@gmail.com>
2023-03-25 21:08:52 -0400

    add button to form

--- src/components/shortenSection/ShortenForm.tsx
+++ src/components/shortenSection/ShortenForm.tsx
@@ -14,7 +14,7 @@ export default function ShortenForm() {
       className="relative flex flex-col w-full p-10 bg-darkViolet roundend-lg md:flex-row md:space-y-0 md:space-x-3">
       <input
         className="flex-1 p-3 border-2 rounded-lg placeholder-yellow-500 focus:outline-none"
-        id="shorten-form" 
+        id="shorten-link" 
         name="shorten-link"
         type={"text"}
         placeholder={"Enter Link Here"}
@@ -23,7 +23,10 @@ export default function ShortenForm() {
         required
       >  
       </input>
-      {/* <button>Submit</button> */}
+      <button
+        className="px-10 py-3 text-white bg-cyan rounded-lg hover:bg-cyanLight focus:outline-none md:py-2" 
+        disabled={false}
+      >Shorten Link</button>
     </form>
   );
 }
