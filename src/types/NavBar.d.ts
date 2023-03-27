95340a788578197aa06da7ba3e07537a55a15ee5
Nick <nsagias@gmail.com>
2023-03-27 19:47:20 -0400

    add nav mobile menu type

--- src/types/NavBar.d.ts
+++ src/types/NavBar.d.ts
@@ -1,11 +1,17 @@
 // A type that describe menu item input
 export type NavBarMenuItemName = { 
   menuItemName: string, 
-  url: string
+  url: string,
 }; 
 
 export type NavBarMenuItemData = {
   id: number,
   menuItemName: string,
-  url: string;
+  url: string,
+};
+
+export type MobileMenuItem = {
+  linkURL: string,
+  className: string,
+  buttonName: string,
 };
