dd7a81d545b2ef2f9e02dd73369a0744ebbaeb4e
Nick <nsagias@gmail.com>
2023-03-27 04:00:05 -0400

    add footer menu to footer

--- src/components/footerSection/Footer.tsx
+++ src/components/footerSection/Footer.tsx
@@ -1,8 +1,45 @@
+import MenuContainer from "./MenuContainer";
+import FooterMenu from "./FooterMenu";
+import { FooterMenuData } from "../../types/Footer";
+import { useState } from "react";
+
+const footerMenuList: FooterMenuData[] = [ 
+  { footerMenuTitle: "Features", footerData: [
+    { URL: "# ", linkName: "Link shortenning"},
+    { URL: "# ", linkName: "Branding links"},
+    { URL: "# ", linkName: "Analytics"}
+  ]}, 
+  { footerMenuTitle: "Resources", footerData: [
+    { URL: "# ", linkName: "Blog"},
+    { URL: "# ", linkName: "Developers"},
+    { URL: "# ", linkName: "Support"}
+  ]},
+  { footerMenuTitle: "Company", footerData: [
+    { URL: "# ", linkName: "About"},
+    { URL: "# ", linkName: "Our Team"},
+    { URL: "# ", linkName: "Careers"},
+    { URL: "# ", linkName: "Contact Us"},
+  ]}
+];
+
 export default function Footer() {
+
+  const [footerData, setFooterData] = useState<FooterMenuData[]>(footerMenuList)
+
+
   return (
     <footer className="py-16 bg-darkerViolet">
       <div className="container flex flex-col items-center justiify-between mx-auto space-y-16 md:flex-row md:space-y-0 md:items-start">
-        
+        {/* LOGO */}
+        <img src="images/logo-tiny-app.svg" alt="" className="w-32 border-8 border-cyan px-4 py-1 pt-2 rounded-full bg-white" />
+
+        {/* Menus Container */}
+        <MenuContainer>
+          <FooterMenu 
+            heading={footerData[0].footerMenuTitle}
+            footerData={footerData[0].footerData} 
+          />
+        </MenuContainer>
       </div>
     </footer>
   );
