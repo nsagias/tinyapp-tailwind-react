5d09c7ace3b5f94ac3e13b65b8393d9e0c110eb8
Nick <nsagias@gmail.com>
2023-03-27 03:57:23 -0400

    create footer types

---
+++ src/types/Footer.d.ts
@@ -0,0 +1,8 @@
+export type FooterMenuData = {
+  footerMenuTitle: string,
+  footerData: { URL: string, linkName: string}[]
+}
+
+export type FooterDataItem = {
+  footerData: { URL: string, linkName: string}
+}
