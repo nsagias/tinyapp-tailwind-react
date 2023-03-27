c844ad2f7446142a6472aa7446789d8c7726c351
Nick <nsagias@gmail.com>
2023-03-26 20:45:03 -0400

    order box image placement

--- src/components/marketing/MarketingFeatures.tsx
+++ src/components/marketing/MarketingFeatures.tsx
@@ -6,19 +6,20 @@ export default function MarketingFeatures() {
       <div className="hidden absolute top-24 w-10/12 left-16 h-3 bg-cyan md:block"></div>
       {/* Vertical Line */}
       <div className="absolute w-2 left-1/2 h-full -ml-1 bg-cyan md:hidden"></div>
+
+
       {/* Marketing Box 1 */}
       <div className="relative flex flex-col p-6 space-y-6 bg-white rounded-lg md:w-1/3">
         {/* Image Wrapper/Position */}
         <div className="absolute -ml-10 left-1/2 -top-10 md:left-16">
           {/* Image Container */}
           <div className="flex items-center justify-center w-20 h-20 p-4 rounded-full bg-darkerViolet">
-            <img src="images/icon-brand-recognition.svg" alt="" className="" />
+            <img src="images/icon-fully-customizable.svg"alt="" className="" />
           </div>
         </div>
           <h5 className="pt-6 text-xl font-bold text-center capitalize md:text-left">Brand Recognition</h5>
           <p className="text-center text-gray-400 md:text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit eum inventore modi dolorem asperiores consequuntur officiis similique, facere porro dolor!</p>
       </div>
-
 
 
       {/* Marketing Box 2 */}
@@ -30,7 +31,7 @@ export default function MarketingFeatures() {
             <img src="images/icon-brand-recognition.svg" alt="" className="" />
           </div>
         </div>
-          <h5 className="pt-6 text-xl font-bold text-center capitalize md:text-left">Brand Recognition</h5>
+          <h5 className="pt-6 text-xl font-bold text-center capitalize md:text-left">Marketing Statistics</h5>
           <p className="text-center text-gray-400 md:text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nostrum dignissimos laboriosam similique cum voluptatibus numquam eius modi incidunt cupiditate.</p>
       </div>
 
@@ -40,16 +41,12 @@ export default function MarketingFeatures() {
         <div className="absolute -ml-10 left-1/2 -top-10 md:left-16">
           {/* Image Container */}
           <div className="flex items-center justify-center w-20 h-20 p-4 rounded-full bg-darkerViolet">
-            <img src="images/icon-brand-recognition.svg" alt="" className="" />
+            <img src="images/icon-detailed-records.svg" alt="" className="" />
           </div>
         </div>
-          <h5 className="pt-6 text-xl font-bold text-center capitalize md:text-left">Brand Recognition</h5>
+          <h5 className="pt-6 text-xl font-bold text-center capitalize md:text-left">Fully Customizable</h5>
           <p className="text-center text-gray-400 md:text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque unde beatae illum et excepturi! Omnis necessitatibus dolor cum quasi quo.</p>
         </div>
-
-
-
-
 
       </div>
     </section>
