fc29a36920b45a04a9ec9b3fb8eec2b7018993d1
Nick <nsagias@gmail.com>
2023-03-27 20:12:05 -0400

    add padding 1

--- src/components/navBar/NavMobileMenu.tsx
+++ src/components/navBar/NavMobileMenu.tsx
@@ -2,7 +2,7 @@ import { MobileMenuItem } from "../../types/NavBar";
 
 export default function NavMobileMenu({ linkURL, styling, buttonName }: MobileMenuItem): JSX.Element {
   return (
-    <div className="flex flex-col items-center justify-center w-full space-y-6 font-bold text-white rounded-sm">
+    <div className="flex flex-col py-1 items-center justify-center w-full space-y-6 font-bold text-white rounded-sm">
       <a href={`${linkURL}`} className={`${styling}`}>{buttonName}</a>
     </div>
   );
