b920a4bf719ed2cefdb3fe91a3baae8bf8e252e7
Nick <nsagias@gmail.com>
2023-04-09 19:21:57 -0400

    add left side header seciton

--- src/components/login/LoginCard.tsx
+++ src/components/login/LoginCard.tsx
@@ -3,7 +3,8 @@ export default function LoginCard({}): JSX.Element {
     <div className="relative flex flex-col m-6 space-y-10 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0 md:m-0">
       {/* Left Side Of Login Card */}
         <div className="p-6 md:p-20">
-here
+          <h2 className="mb-5 text-4xl font-bold">Login</h2>
+          <p className="max-w-sm mb-12 font-sans font-bold text-grayViolet">Login to your account.</p>
         </div>
       {/* Right Side Of Login Card */}
       <img src="images/water-bubles.jpg" alt="" className="w-[430px] hidden md:block" />
