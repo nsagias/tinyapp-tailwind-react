7d3814167067fde0107d918306283aa4ef8e7599
Nick <nsagias@gmail.com>
2023-04-09 19:14:22 -0400

    create left side and rights side, import bubbles image

--- src/components/login/LoginCard.tsx
+++ src/components/login/LoginCard.tsx
@@ -1,5 +1,12 @@
 export default function LoginCard({}): JSX.Element {
   return (
-    <h1>here</h1>
+    <div className="relative flex flex-col m-6 space-y-10 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0 md:m-0">
+      {/* Left Side Of Login Card */}
+        <div className="p-6 md:p-20">
+here
+        </div>
+      {/* Right Side Of Login Card */}
+      <img src="images/water-bubles.jpg" alt="" className="w-[430px] hidden md:block" />
+    </div>
   );
 }
