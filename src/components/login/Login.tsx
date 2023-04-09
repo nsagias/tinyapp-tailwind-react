92b06d8677df0e26fb20bf1c3cc6d45ce3cd6884
Nick <nsagias@gmail.com>
2023-04-09 18:52:57 -0400

    create login and longCard compeonts

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,10 @@
+
+// Hero({ authorized}: {authorized: boolean}):JSX.Element
+export default function Login({}): JSX.Element {
+
+  return(
+    <div className="flex items-center justify-center min-h-screen bg-rose-50">
+
+    </div>
+  );
+}
---
+++ src/components/login/LoginCard.tsx
@@ -0,0 +1,5 @@
+export default function LoginCard({}): JSX.Element {
+  return (
+    <h1>here</h1>
+  );
+}
