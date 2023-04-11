e3f422a28dfba4df8cdae2ec52024b008767b9c9
Nick <nsagias@gmail.com>
2023-04-11 16:52:04 -0400

    update folder name and imports

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,6 @@
 import CTASection from "../../components/CTASection/CTASection";
 import HeroLandingPage from "../../components/heroSection/HeroHomeSection";
-import MarketingSectionContainer from "../../components/marketing/MarketingSectionContainer";
+import MarketingSectionContainer from "../../components/marketingSection/MarketingSectionContainer";
 
 export default function HomeContainer({}): JSX.Element {
   return (
