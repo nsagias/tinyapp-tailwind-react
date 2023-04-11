6a403e8b4af18fdc2ba3ba4b0f74a14d7fe9ce02
Nick <nsagias@gmail.com>
2023-04-10 23:36:52 -0400

    add react router dom Link

--- src/components/navBar/NavMobileMenu.tsx
+++ src/components/navBar/NavMobileMenu.tsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
 import { MobileMenuItem } from "../../types/NavBar";
 
 export default function NavMobileMenu({ linkURL, styling, buttonName }: MobileMenuItem): JSX.Element {
   return (
     <div className="flex flex-col py-1 items-center justify-center w-full space-y-6 font-bold text-white rounded-sm">
-      <a href={`${linkURL}`} className={`${styling}`}>{buttonName}</a>
+      {/* <a href={`${linkURL}`} className={`${styling}`}>{buttonName}</a> */}
+      <Link 
+        className={`${styling}`}
+        to={`${linkURL}`}
+        >{buttonName}
+      </Link>
     </div>
   );
 }
