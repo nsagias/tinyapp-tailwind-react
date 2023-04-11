4f28854671914aec0f6d001ddf51e95f6acd3f41
Nick <nsagias@gmail.com>
2023-04-11 19:23:06 -0400

    revert

--- src/components/registrationSection/RegistrationForm.tsx
+++ src/components/registrationSection/RegistrationForm.tsx
@@ -23,7 +23,7 @@ export default function RegistrationForm({}): JSX.Element {
   };
 
   return (
-    <div className="relative flex flex-col m-6 space-y-10 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0 md:m-0">
+    <div className="relative flex flex-col space-y-10 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0 md:m-0">
       {/* Left Side Of Login Card */}
         <div className="p-6 md:p-20">
           {/* Top of card */}
