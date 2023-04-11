9e6b5129cf35129fbb6d23f87b346b14d2826e0e
Nick <nsagias@gmail.com>
2023-04-10 23:39:45 -0400

    add Link to home to main logo

--- src/components/navBar/NavLogo.tsx
+++ src/components/navBar/NavLogo.tsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function NavLogo() {
   return (
     <>
     {/* todo add onClick/routing to home */}
+    
       <button>
-        <img src="images/logo-tiny-app.svg" alt="" className="w-32 border-8 border-cyan px-4 py-1 pt-2 rounded-full hover:opacity-70" />
+        <Link 
+          to={"/"}> <img src="images/logo-tiny-app.svg" 
+          alt="" 
+          className="w-32 border-8 border-cyan px-4 py-1 pt-2 rounded-full hover:opacity-70" />
+        </Link>
       </button>
     </>
 
