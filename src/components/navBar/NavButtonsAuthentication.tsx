97c09c398f03a4ef5928fed59b08163f92fbacfc
Nick <nsagias@gmail.com>
2023-04-11 18:11:02 -0400

    add links to login and register

--- src/components/navBar/NavButtonsAuthentication.tsx
+++ src/components/navBar/NavButtonsAuthentication.tsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function NavButtonsAuthentication({ authorized }: { authorized : boolean}) {
   return (
     <>
       {!authorized ? 
         (
           <>
-            <a href="#" className="hover:text-darkerViolet">Login</a>
-            <a href="#" className="px-8 py-3 font-bold text-white bg-cyan rounded-full hover:opacity-70">Sign Up</a>
-          </>      
+            <Link to={"/login"} className="hover:text-darkerViolet">Login</Link>
+            <Link to={"/register"} className="px-8 py-3 font-bold text-white bg-cyan rounded-full hover:opacity-70" >Sign Up</Link>
+            </>      
         ) : (
           <a href="#" className="px-8 py-3 font-bold text-white bg-cyan rounded-full hover:opacity-70">Logout</a>
         )
