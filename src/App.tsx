88d4fabf15c752fd135a79a26f53a6425bed0f4e
Nick <nsagias@gmail.com>
2023-04-10 23:40:27 -0400

    add routes to existing paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 import {
   BrowserRouter,
   createBrowserRouter,
@@ -8,29 +8,35 @@ import {
   Routes,
   Link,
 } from "react-router-dom";
-import CTASection from './components/CTASection/CTASection';
-import Footer from './components/footerSection/Footer';
-import Hero from './components/heroSection/Hero';
-import MarketingSectionContainer from './components/marketing/MarketingSectionContainer';
-import NavBar from './components/navBar/NavBar';
-import ShortenSection from './components/shortenSection/ShortenSection';
-import Login from './components/loginSection/LoginContainer';
-import RegisterContainter from './components/registrationSection/RegistrationContainer';
-import HomeContainer from './components/home/HomeContainer';
+import Footer from "./components/footerSection/Footer";
+import NavBar from "./components/navBar/NavBar";
+import ShortenSection from "./components/shortenSection/ShortenSection";
+import Login from "./components/loginSection/LoginContainer";
+import RegistrationContainter from "./components/registrationSection/RegistrationContainer";
+import HomeContainer from "./components/home/HomeContainer";
+import Features from "./components/features/Features";
+import Pricing from "./components/pricing/Pricing";
+import Docs from "./components/documentation/Docs";
 
 export default function App() {
 
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
   return (
     <div className="App">
       <NavBar authorized={isLoggedIn} />
-      <Hero authorized={isLoggedIn} />
-      {/* <Login /> */}
-      {/* <RegisterContainter /> */}
-      {/* <ShortenSection /> */}
-      <HomeContainer />
+        <Routes>
+        
+          <Route path="/features" element={<Features />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/docs" element={ <Docs />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<RegistrationContainter />} />
+          <Route path="/shorturls" element={ <ShortenSection />} />
+          <Route path="/" element={<HomeContainer isLoggedIn={isLoggedIn} />} /> 
+
+        </Routes>
       <Footer authorized={true}/>
-    <Routes></Routes>
     </div>
   );
 }
