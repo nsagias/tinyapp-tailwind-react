9af01a53b5f667ed1da75aa7b3a0ce023443ef01
Nick <nsagias@gmail.com>
2023-04-10 23:38:23 -0400

    add rero section

--- src/components/home/HomeContainer.tsx
+++ src/components/home/HomeContainer.tsx
@@ -1,9 +1,11 @@
 import CTASection from "../CTASection/CTASection";
+import Hero from "../heroSection/Hero";
 import MarketingSectionContainer from "../marketing/MarketingSectionContainer";
 
-export default function HomeContainer(): JSX.Element {
+export default function HomeContainer({isLoggedIn}: { isLoggedIn: boolean}): JSX.Element {
   return (
     <>
+    <Hero authorized={isLoggedIn} />
     <MarketingSectionContainer />
     <CTASection />
     </>
