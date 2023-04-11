a7e19b96bbd1d8d96ceab98fe58961d72a480544
Nick <nsagias@gmail.com>
2023-04-10 23:36:28 -0400

    add react router dom Link

--- src/components/navBar/NavMenuItem.tsx
+++ src/components/navBar/NavMenuItem.tsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
 import { NavBarMenuItemName } from "../../types/NavBar";
 
 
 export default function NavMenuItem({ menuItemName, url} : NavBarMenuItemName): JSX.Element {
   return (
-    <a href={url} className="text-grayViolet hover:text-darkerViolet">{menuItemName}</a>
+    <>
+      {/* <a href={url} className="text-grayViolet hover:text-darkerViolet">{menuItemName}</a> */}
+      <Link 
+        className="text-grayViolet hover:text-darkerViolet" 
+        to={url}>{menuItemName}
+      </Link>
+    </>
   );
 }
