11a8c81c529701febc77ec037f17c3965b1ba7e0
Nick <nsagias@gmail.com>
2023-04-12 03:15:57 -0400

    create input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,27 @@
-export default function Input({}): JSX.Element {
+
+export interface InputProps {
+  classname?: string;
+  isValid?: boolean;
+  inValid?: string;
+  label?: string;
+  type?: string;
+  id?: string;
+  value?: string;
+  changeHandler?: () => void;
+  validationHandler?: () => void;
+};
+
+export default function Input({classname, isValid, inValid, label, type, id, value, changeHandler, validationHandler}: InputProps ): JSX.Element {
   return (
-    <>input component</>
+    <div className={`${classname} ${isValid === false ? inValid : ""} `}>
+      <label htmlFor={id}>{label}</label>
+      <input 
+        type={type}
+        id={id}
+        value={value}
+        onChange={changeHandler}
+        onBlur={validationHandler}
+      />
+    </div>
   );
 }
