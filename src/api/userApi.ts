63f33c58916f60b6e437397a7d99316c5467a5c4
Nick <nsagias@gmail.com>
2023-04-14 17:30:41 -0400

    add success response for user

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,25 +1,25 @@
 import axios, { AxiosInstance } from "axios";
-import { LoginUser, LoginUserErrorResponse, LoginUserSuccessResponse, Logout, LogoutResponse, RegisterUser, UserInfo } from "../types/api/userApi";
-import { Token } from "aws-sdk/clients/cloudwatchlogs";
+import { LoginUser, LoginUserSuccessResponse, Logout, LogoutResponse, RegisterUser, RegisterUserSuccessResponse, UserErrorResponse, UserInfo } from "../types/api/userApi";
+
 
 const userApi: AxiosInstance = axios.create({
   baseURL: `${process.env.USER_URL}:${process.env.USER_URL_PORT}`
 });
 
 
-export const registerUser = async (userData: RegisterUser) => {
+export const registerUser = async (userData: RegisterUser): Promise< RegisterUserSuccessResponse| UserErrorResponse> => {
   const response = await userApi.post("/register", userData);
   return response.data;
 };
 
 
-export const loginUser = async (userData: LoginUser): Promise<LoginUserSuccessResponse | LoginUserErrorResponse> => {
+export const loginUser = async (userData: LoginUser): Promise<LoginUserSuccessResponse | UserErrorResponse> => {
   const response = await userApi.post("/login", userData);
   return response.data;
 };
 
 
-export const logoutUser = async (userData: Logout): Promise<LogoutResponse> => {
+export const logoutUser = async (userData: Logout): Promise<LogoutResponse | UserErrorResponse> => {
   const response = await userApi.post("/logout", userData);
   return response.data;
 };
--- src/types/api/userApi.d.ts
+++ src/types/api/userApi.d.ts
@@ -11,11 +11,21 @@ export type UserInfo = {
   updatedAt?: Date,
 }
 
+export type UserErrorResponse = {
+  message: string
+}
+
+// Registration Section
 export type RegisterUser = {
   firstName: string,
   lastName: string,
   email: string,
   password: string,
+}
+
+export type RegisterUserSuccessResponse = {
+  token: Token;
+  userInfo: UserInfo;
 }
 
 // Login User Section
@@ -27,9 +37,6 @@ export type LoginUser = {
 export type LoginUserSuccessResponse = {
   token: Token;
   userInfo: UserInfo;
-}
-export type LoginUserErrorResponse = {
-  message: string
 }
 
 
