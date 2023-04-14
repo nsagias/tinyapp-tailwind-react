39f6c18773faabe375e3f1326c063c2576995b43
Nick <nsagias@gmail.com>
2023-04-14 18:29:42 -0400

    refactor types

--- src/api/linkApi.ts
+++ src/api/linkApi.ts
@@ -1,71 +1,44 @@
-import { Token } from "aws-sdk/clients/cloudwatchlogs";
 import axios, { AxiosInstance } from "axios";
-import { UrlModel, LinkErrorMessage, LinkSuccessReponse } from "../types/api/linkApi";
+import { 
+  DeleteShortLink,
+  DeleteShortLinkResponse,
+  GetLinksByUserId,
+  GetLongURL,
+  LinkErrorMessage,
+  LinksListSuccessReponse,
+  LinkSuccessReponse,
+  NewShortLink,
+  UpdateShortLink
+} from "../types/api/linkApi";
 
 const linkApi: AxiosInstance = axios.create({
   baseURL: `${process.env.LINK_URL}:${process.env.LINK_URL_PORT}`
 });
 
 
-export type GetLinksByUserId = {
-  token: Token,
-  userId: number,
-}
-
-export type LinksByUserIdSuccessReponse = {
-  message: string,  
-  data: UrlModel[],
-}
-
-export const getLinksByUserId = async (params: GetLinksByUserId): Promise<LinksByUserIdSuccessReponse | LinkErrorMessage> => {
+export const getLinksByUserId = async (params: GetLinksByUserId): Promise<LinksListSuccessReponse | LinkErrorMessage> => {
   const response = await linkApi.get(`/urls/users/${params.userId}`, {data: params});
   return response.data;
 };
 
 
-export type NewShortLink = {
-  token: Token,
-  userId: number,
-  longUrl: string
-}
-
-export const getCreateShortLink = async (params: NewShortLink ): Promise<LinkSuccessReponse | LinkErrorMessage> => {
-  const response = await linkApi.post(`/urls/new`, {data: params}) ;
+export const getCreateShortLink = async (params: NewShortLink): Promise<LinkSuccessReponse | LinkErrorMessage> => {
+  const response = await linkApi.post(`/urls/new`, {data: params});
   return response.data;
 };
 
-export type UpdateShortLink = {
-  token: Token,
-  userId: number,
-  shortUrl: string,
-  updatedUrlData: UrlModel
-}
+
 export const updateShortLinkByUserId = async (params: UpdateShortLink): Promise<LinkSuccessReponse | LinkErrorMessage> => {
-  const response = await linkApi.patch(`/urls/delete`, {data: params}) ;
+  const response = await linkApi.patch(`/urls/delete`, {data: params});
   return response.data;
 };
 
 
-export type DeleteShortLink = {
-  token: Token,
-  userId: number,
-  shortUrl: string,
-}
-
-export type DeleteShortLinkResponse = {
-  message: boolean
-}
 export const deleteShorLinkByUserId = async (params: DeleteShortLink): Promise<DeleteShortLinkResponse | LinkErrorMessage> => {
   const response = await linkApi.delete("/urls/delete", {data: params});
   return response.data;
 };
 
-
-export type  GetLongURL = {
-  token: Token,
-  userId: number,
-  shortUrl: string,
-}
 
 export const getLinkByUserIdAndShortLink = async (params: GetLongURL): Promise<LinkSuccessReponse | LinkErrorMessage> => {
   const response = await linkApi.get(`/urls/users/${params.userId}/shortUrl/${params.shortUrl}`, {data: params});
--- src/types/api/linkApi.d.ts
+++ src/types/api/linkApi.d.ts
@@ -18,3 +18,42 @@ export type LinkErrorMessage = {
   message: string
 }
 
+export type GetLinksByUserId = {
+  token: Token,
+  userId: number,
+}
+
+export type LinksListSuccessReponse = {
+  message: string,  
+  data: UrlModel[],
+}
+
+export type NewShortLink = {
+  token: Token,
+  userId: number,
+  longUrl: string
+}
+
+export type UpdateShortLink = {
+  token: Token,
+  userId: number,
+  shortUrl: string,
+  updatedUrlData: UrlModel
+}
+
+
+export type DeleteShortLink = {
+  token: Token,
+  userId: number,
+  shortUrl: string,
+}
+
+export type DeleteShortLinkResponse = {
+  message: boolean
+}
+
+export type  GetLongURL = {
+  token: Token,
+  userId: number,
+  shortUrl: string,
+}
