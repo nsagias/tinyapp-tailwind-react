a95150e9d346cf255f6ce1cd5fa7939b952ceb23
Nick <nsagias@gmail.com>
2023-04-14 17:18:12 -0400

    add response data types

--- src/types/api/userApi.d.ts
+++ src/types/api/userApi.d.ts
@@ -1,3 +1,16 @@
+export type UserInfo = {
+  id?: number,
+  firstName?: string,
+  lastName?: string,
+  email?: string,
+  emailVerified?: Date | null,
+  password?: string,
+  active?: boolean,
+  deletedAt?: Date | null,
+  createdAt?: Date,
+  updatedAt?: Date,
+}
+
 export type RegisterUser = {
   firstName: string,
   lastName: string,
@@ -5,11 +18,27 @@ export type RegisterUser = {
   password: string,
 }
 
+// Login User Section
 export type LoginUser = {
   email: string,
   password: string,
 }
 
+export type LoginUserSuccessResponse = {
+  token: Token;
+  userInfo: UserInfo;
+}
+export type LoginUserErrorResponse = {
+  message: string
+}
+
+
+// Logout User Section
 export type Logout = {
   authToken: string;
 }
+
+export type LogoutResponse = {
+  message: string
+}
+
