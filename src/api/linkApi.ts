d127b2227f5dc67f05debb80e2609dd1197c4496
Nick <nsagias@gmail.com>
2023-04-17 01:00:39 -0400

    add types to axios directly

--- src/api/linkApi.ts
+++ src/api/linkApi.ts
@@ -15,31 +15,32 @@ import {
 const linkApi: AxiosInstance = axios.create();
 
 
-export const getLinksByUserId = async (id: UserId, headers: any): Promise<LinksListSuccessReponse | LinkErrorMessage> => {
-  const response = await linkApi.get(`/urls/users/${id}`, { headers } );
+export const getLinksByUserId = async (id: any): Promise<LinksListSuccessReponse> => {
+  // const response = await linkApi.get(`/urls/users/${id}`, { headers } );
+  const response = await linkApi.get<LinksListSuccessReponse>(`/urls/users/${id}` );
   return response.data;
 };
 
 
-export const getCreateShortLink = async (req: NewShortLink, headers: any): Promise<LinkSuccessReponse | LinkErrorMessage> => {
-  const response = await linkApi.post(`/urls/new`, { req, headers}) ;
+export const getCreateShortLink = async (req: NewShortLink, headers: any): Promise<LinkSuccessReponse> => {
+  const response = await linkApi.post<LinkSuccessReponse>(`/urls/new`, { req, headers}) ;
   return response.data;
 };
 
 
-export const updateShortLinkByUserId = async (req: UpdateShortLink, headers: any): Promise<LinkSuccessReponse | LinkErrorMessage> => {
-  const response = await linkApi.patch(`/urls/delete`, { req, headers }) ;
+export const updateShortLinkByUserId = async (req: UpdateShortLink, headers: any): Promise<LinkSuccessReponse> => {
+  const response = await linkApi.patch<LinkSuccessReponse>(`/urls/delete`, { req, headers }) ;
   return response.data;
 };
 
 
-export const deleteShorLinkByUserId = async (req: DeleteShortLink, headers: any): Promise<DeleteShortLinkResponse | LinkErrorMessage> => {
-  const response = await linkApi.delete("/urls/delete", {data: req, headers});
+export const deleteShorLinkByUserId = async (req: DeleteShortLink, headers: any): Promise<DeleteShortLinkResponse> => {
+  const response = await linkApi.delete<DeleteShortLinkResponse>("/urls/delete", {data: req, headers});
   return response.data;
 };
 
 
-export const getLinkByUserIdAndShortLink = async (req: GetLongURL, headers: any): Promise<LinkSuccessReponse | LinkErrorMessage> => {
-  const response = await linkApi.get(`/urls/users/${req.userId}/shortUrl/${req.shortUrl}`, { headers });
+export const getLinkByUserIdAndShortLink = async (req: GetLongURL, headers: any): Promise<LinkSuccessReponse> => {
+  const response = await linkApi.get<LinkSuccessReponse>(`/urls/users/${req.userId}/shortUrl/${req.shortUrl}`, { headers });
   return response.data;
 };
