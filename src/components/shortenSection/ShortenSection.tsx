ead1cf43dda753655088b412da775344f0f0b84c
Nick <nsagias@gmail.com>
2023-04-17 02:29:05 -0400

    update to use locale storage token

--- src/components/shortenSection/ShortenSection.tsx
+++ src/components/shortenSection/ShortenSection.tsx
@@ -5,30 +5,36 @@ import ShortenForm from "./ShortenForm";
 import ShortenSectionContainer from "./ShortenSectionContainer";
 import axios from "axios";
 import { getLinksByUserId } from "../../api/linkApi";
+import { LinksListSuccessReponse } from "../../types/api/linkApi";
   
+
+
 const shortenLinkData: ShortLinkData[] = [
-  { id: 1, fullLink: "https://somecoolwebsite.com", shortLink: "https//tinyapp.ink/k4Ikyk" },
-  { id: 2, fullLink: "https://anothercoolwebsite.com", shortLink: "https//tinyapp.ink/AbCdefk" },
-  { id: 3, fullLink: "https://bestcoolwebsite.com", shortLink: "https//tinyapp.ink/Z24Ikyk" },
+  { id: 1, fullLink: "https://somecoolwebsite.com", shortLink: `k4Ikyk` },
+  { id: 2, fullLink: "https://anothercoolwebsite.com", shortLink: `AbCdefk` },
+  { id: 3, fullLink: "https://bestcoolwebsite.com", shortLink: `Z24Ikyk` },
 ];
 
+
+
 export default function ShortenSection() {
-  const [shortenLinks, setShortLinks] = useState<ShortLinkData[]>([]);
+  const [shortenLinks, setShortLinks] = useState<any>([]);
+  const [token, setToken] = useState<string>("")
 
   // TODO: Add new shorten link call to depency array.
-  useEffect(() => {
+  useEffect( () => {
     getLatestData();
   }, []);
 
   // TODO: Refactor
   const getLatestData = async() => {
     // TODO: Update with axios call 
-    const data: ShortLinkData[] = await shortenLinkData;
-    await setShortLinks(data);
+ 
+    const token = localStorage.getItem("token")
+    const data = await getLinksByUserId(1, token!)
+  
 
   };
-
-
 
   return (
     <section id="shorten-section" className="relative bg-gray-100 pb-10">
