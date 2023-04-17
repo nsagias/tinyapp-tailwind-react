5cb57f32bb8db4cc706c685c7a13654c0aa1c0f5
Nick <nsagias@gmail.com>
2023-04-17 02:33:47 -0400

    update to pass token to each route

--- src/api/linkApi.ts
+++ src/api/linkApi.ts
@@ -15,32 +15,41 @@ import {
 const linkApi: AxiosInstance = axios.create();
 
 
-export const getLinksByUserId = async (id: any): Promise<LinksListSuccessReponse> => {
-  // const response = await linkApi.get(`/urls/users/${id}`, { headers } );
-  const response = await linkApi.get<LinksListSuccessReponse>(`/urls/users/${id}` );
+export const getLinksByUserId = async (id: any, token: string): Promise<LinksListSuccessReponse> => {
+  const response = await linkApi.get(`/urls/users/${id}`, { headers: {
+    "Authorization": token
+  }});
   return response.data;
 };
 
 
-export const getCreateShortLink = async (req: NewShortLink, headers: any): Promise<LinkSuccessReponse> => {
-  const response = await linkApi.post<LinkSuccessReponse>(`/urls/new`, { req, headers}) ;
+export const getCreateShortLink = async (req: NewShortLink, token: string): Promise<LinkSuccessReponse> => {
+  const response = await linkApi.post<LinkSuccessReponse>(`/urls/new`, { req, headers: {
+    "Authorization": token
+  }});
   return response.data;
 };
 
 
-export const updateShortLinkByUserId = async (req: UpdateShortLink, headers: any): Promise<LinkSuccessReponse> => {
-  const response = await linkApi.patch<LinkSuccessReponse>(`/urls/delete`, { req, headers }) ;
+export const updateShortLinkByUserId = async (req: UpdateShortLink, token: string): Promise<LinkSuccessReponse> => {
+  const response = await linkApi.patch<LinkSuccessReponse>(`/urls/delete`, { req, headers: {
+    "Authorization": token
+  }});
   return response.data;
 };
 
 
-export const deleteShorLinkByUserId = async (req: DeleteShortLink, headers: any): Promise<DeleteShortLinkResponse> => {
-  const response = await linkApi.delete<DeleteShortLinkResponse>("/urls/delete", {data: req, headers});
+export const deleteShorLinkByUserId = async (req: DeleteShortLink, token: string): Promise<DeleteShortLinkResponse> => {
+  const response = await linkApi.delete<DeleteShortLinkResponse>("/urls/delete", {data: req, headers: {
+    "Authorization": token
+  }});
   return response.data;
 };
 
 
-export const getLinkByUserIdAndShortLink = async (req: GetLongURL, headers: any): Promise<LinkSuccessReponse> => {
-  const response = await linkApi.get<LinkSuccessReponse>(`/urls/users/${req.userId}/shortUrl/${req.shortUrl}`, { headers });
+export const getLinkByUserIdAndShortLink = async (req: GetLongURL, token: string): Promise<LinkSuccessReponse> => {
+  const response = await linkApi.get<LinkSuccessReponse>(`/urls/users/${req.userId}/shortUrl/${req.shortUrl}`, { headers: {
+    "Authorization": token
+  }});
   return response.data;
 };
