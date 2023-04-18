6298ba078ca309902dba529b5083fd12a057703e
Nick <nsagias@gmail.com>
2023-04-18 01:27:36 -0400

    create token initial state and const

---
+++ src/store/constants/token.ts
@@ -0,0 +1,10 @@
+import { Token } from "../../types/api/token";
+
+export const INITIAL_STATE_USER: Token = {
+  id: 0,
+  token: "",
+  userId: 0,
+  createdAt: undefined
+};
+
+export const TOKEN: string = "TOKEN";
