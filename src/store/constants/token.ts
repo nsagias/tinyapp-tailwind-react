4590dcd720d7b1d9df6937e19002d4875d52fb43
Nick <nsagias@gmail.com>
2023-04-18 01:30:00 -0400

    remove unused token parts

--- src/store/constants/token.ts
+++ src/store/constants/token.ts
@@ -1,10 +1,8 @@
 import { Token } from "../../types/api/token";
 
 export const INITIAL_STATE_USER: Token = {
-  id: 0,
   authToken: "",
   userId: 0,
-  createdAt: undefined
 };
 
 export const TOKEN: string = "TOKEN";
