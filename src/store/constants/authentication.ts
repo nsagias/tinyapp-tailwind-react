4d4c62d9544e5db00de8cea70f7423da3acb929e
Nick <nsagias@gmail.com>
2023-04-19 00:02:59 -0400

    refactor types

--- src/store/constants/authentication.ts
+++ src/store/constants/authentication.ts
@@ -1,6 +1,4 @@
-type IAuthentication = {
-  isAuthenticated: boolean
-} 
+import { IAuthentication } from "../types/authentication";
 
 export const INITIAL_STATE_AUTHENTICATION: IAuthentication = { isAuthenticated: false };
 export const AUTHENTICATION: string = "AUTHENTICATION";
