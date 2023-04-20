9074761b7095f66f4d62dab92832b666ae4a2ffb
Nick <nsagias@gmail.com>
2023-04-19 20:49:11 -0400

    import IAuthentication from type

--- src/store/constants/authentication.ts
+++ src/store/constants/authentication.ts
@@ -1,4 +1,4 @@
-import { IAuthentication } from "../types/authentication";
+import { IAuthentication } from "../../types/store/authentication";
 
 export const INITIAL_STATE_AUTHENTICATION: IAuthentication = { isAuthenticated: false };
 export const AUTHENTICATION: string = "AUTHENTICATION";
