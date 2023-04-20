4574f2bb9242fe893324a53772ba8dc75fe18194
Nick <nsagias@gmail.com>
2023-04-20 19:53:08 -0400

    add comments and return types

--- src/services/localStorageService.ts
+++ src/services/localStorageService.ts
@@ -1,42 +1,93 @@
 // local storage api
 const localStorageService = {
-  setLocalStorageItem(key: string, value: string) {
+
+  /**
+   * Sets a new locale storage time in the browser
+   * @param key string
+   * @param value string
+   */
+  setLocalStorageItem(key: string, value: string): void {
     localStorage.setItem(key, value);
   },
 
+  /**
+   * Get local storage item with key
+   * @param key string
+   * @returns value
+   */
   getLocalStorageItem(key: string): string | null {
     return localStorage.getItem(key);
   },
-  removeLocalStorageItem(key: string) {
+
+  /**
+   * Removes local storage item using key
+   * @param key
+   */
+  removeLocalStorageItem(key: string): void {
     localStorage.removeItem(key);
   },
-  getLocalStorageItemByIndex(index: number) {
-    return localStorage.key(index);
+
+  /**
+   * Gets local storage item with index
+   * @param index number
+   * @returns string value
+   */
+  getLocalStorageItemByIndex(index: number): string {
+    return localStorage.key(index)!;
   },
-  clearLocalStorage() {
+
+  /**
+   * Clear local storage remove all data from local storage
+   */
+  clearLocalStorage(): void {
     localStorage.clear();
   },
-  localStorageLength() {
+
+  /**
+   * Gets current length of local storage
+   * @returns number
+   */
+  localStorageLength(): number {
     return localStorage.length;
   },
-  getAllLocalStorageData() {
-    localStorage;
+
+  /**
+   * Get all the data in local storage and returns it
+   */
+  getAllLocalStorageData(): Storage {
+    return localStorage;
   },
+
+  /**
+   * Sets and array value as a string in local storage
+   * @param key string
+   * @param arrayValue array
+   */
   setLocalStorageAsArrayItem(key: string, arrayValue: string[]) {
     if (!Array.isArray(arrayValue))  {
       throw new Error("value must be an array");
     }
    localStorageService.setLocalStorageItem(key, JSON.stringify(arrayValue));
   },
-  getLocalStorageArrayItem(key: string) {
+
+  /**
+   * Gets item by key that must be an array
+   * get string value and converts to array
+   * @param key string
+   * @returns array
+   */
+  getLocalStorageArrayItem(key: string): Array<any> | null {
     const result = localStorageService.getLocalStorageItem(key) || null;
-    if (!result) {
-      return null;
-    }
+    if (!result) return null;
     return JSON.parse(result);
   },
-  populateLocalStorageItems(object: any) {
-    for (const [key, value] of Object.entries(object) as any) {
+  
+  /**
+   * Populates local storage
+   * @param object 
+   */
+  populateLocalStorageItems(object: Storage): void {
+    for (const [key, value] of Object.entries(object)) {
       localStorageService.setLocalStorageItem(key, value);
     };
   }
