4427cb15a52e1e710a1627f85ae7da951bddac64
Nick <nsagias@gmail.com>
2023-04-20 17:31:44 -0400

    add local storage length, get all data, and setLocaleStroage as asArray

--- src/services/localStorageService.ts
+++ src/services/localStorageService.ts
@@ -1,21 +1,37 @@
+import { error } from "console";
 
 // local storage api
 const localStorageService = {
   setLocalStorageItem(key: string, value: string) {
     localStorage.setItem(key, value);
   },
+
   getLocalStorageItem(key: string) {
     localStorage.getItem(key);
   },
   removeLocalStorageItem(key: string) {
     localStorage.removeItem(key);
   },
-  clearLocalStorage() {
-    localStorage.clear();
-  },
   getLocalStorageItemByIndex(index: number) {
     localStorage.key(index);
   },
+  clearLocalStorage() {
+    localStorage.clear();
+  },
+  localStorageLength() {
+    localStorage.length;
+  },
+  getAllLocalStorageData() {
+    localStorage;
+  },
+  setLocaleStorageAsArray(key: string, arrayValue: string[]) {
+    if (!Array.isArray(arrayValue))  {
+      throw new Error("Must be array");
+    }
+    localStorageService.setLocalStorageItem(key, JSON.stringify(arrayValue));
+  },
+  
+
 };
 
 export default localStorageService;
