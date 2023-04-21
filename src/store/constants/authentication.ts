76a7d548383cd5bd379666f0ceba68d5a35ed1e4
Nick <nsagias@gmail.com>
2023-04-21 00:08:43 -0400

    move isAuthenticated to constant

--- src/store/constants/authentication.ts
+++ src/store/constants/authentication.ts
@@ -1,4 +1,2 @@
-import { IAuthentication } from "../../types/store/authentication";
-
-export const INITIAL_STATE_AUTHENTICATION: IAuthentication = { isAuthenticated: false };
 export const AUTHENTICATION: string = "AUTHENTICATION";
+export const isAuthenticated: string = "isAuthenticated";
