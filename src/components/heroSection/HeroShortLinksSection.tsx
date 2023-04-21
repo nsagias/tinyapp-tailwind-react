afad6fee57303eee709ca68e1c526f957001e592
Nick <nsagias@gmail.com>
2023-04-21 18:56:57 -0400

    format and change text

--- src/components/heroSection/HeroShortLinksSection.tsx
+++ src/components/heroSection/HeroShortLinksSection.tsx
@@ -8,37 +8,26 @@ import HeroContainerShortLink from "./components/HeroContainerShortLink";
 
 export default function HeroShortLinksSection({}):JSX.Element {
   return (
-    <section id="hero">
+    <section id="hero-short-links-section">
       {/* Hero Container ShortLink */}
         <HeroContainerShortLink>
-
-
           {/* Content Container */}
           <HeroContentContainerShortLink>
-
-    
             {/* Title */}
             <>
-            <h3 className="text-5xl font-bold text-center lg:text-6xl">Link Stats</h3>
-            {/* Marketing  brangind text*/}
-            <p className="text-2xl text-center text-gray-400 ">Click count: 232322</p>
+              <h3 className="text-5xl font-bold text-center lg:text-6xl">Link Stats</h3>
+              {/* Marketing  brangind text*/}
+              <p className="text-2xl text-center text-gray-400 ">Click count: 232322</p>
             </>
-
             {/* Button */}
             <HeroButtonsContainer>
-            {/* TODO ADD onClick and routing */}
-        
-            <Link to={"/shorturls"} className="py-5 px-10 text-xl font-bold text-white bg-cyan rounded-full lg:py-4 hover:opacity-70">go to short urls</Link>
+              {/* TODO ADD onClick and routing */}
+              <Link to={"/shorturls"} className="py-5 px-10 text-xl font-bold text-white bg-cyan rounded-full lg:py-4 hover:opacity-70">Return To Short Links</Link>
             </HeroButtonsContainer>
-           
-
           </HeroContentContainerShortLink>
-
           {/* Hero Image */}
           <HeroImage />
         </HeroContainerShortLink>
-          
-     
     </section>
   );
 }
