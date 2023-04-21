d871c1a15e279a543a8d3e1d605190fb98d335ad
Nick <nsagias@gmail.com>
2023-04-21 16:55:27 -0400

    add isAuthentication

--- src/store/constants/authentication.ts
+++ src/store/constants/authentication.ts
@@ -2,4 +2,4 @@ export const AUTHENTICATION: string = "AUTHENTICATION";
 export const isAuthenticated: string = "isAuthenticated";
 
 // Local storage removal list
-export const logoutStorageList = ["active","email", "emailVerified", "firstName", "lastName", "id", "token"];
+export const logoutStorageList = ["active","email", "emailVerified", "firstName", "lastName", "id", "token", "isAuthenticated"];
