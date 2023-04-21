61df49f3a3547e36b33fdbf390bd279a9c6b8593
Nick <nsagias@gmail.com>
2023-04-21 03:03:31 -0400

    add logout handler

--- src/components/navBar/NavButtonsAuthentication.tsx
+++ src/components/navBar/NavButtonsAuthentication.tsx
@@ -1,6 +1,21 @@
 import { Link } from "react-router-dom";
+import localStorageService from "../../services/localStorageService";
+import { useDispatch } from "react-redux";
+import { setAuthFalse } from "../../store/slices/authenticationSlice";
+
+
+const logoutStorageList = [
+  "active","email", "emailVerified", "firstName", "lastName", "id", "token"];
+
 
 export default function NavButtonsAuthentication({ authorized }: { authorized : boolean}) {
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {  
+    dispatch(setAuthFalse);
+    localStorageService.removeItemsFromLocalStorage(logoutStorageList);
+  };
+
   return (
     <>
       {!authorized ? 
@@ -10,7 +25,13 @@ export default function NavButtonsAuthentication({ authorized }: { authorized : 
             <Link to={"/register"} className="px-8 py-3 font-bold text-white bg-cyan rounded-full hover:opacity-70" >Sign Up</Link>
             </>      
         ) : (
-          <a href="#" className="px-8 py-3 font-bold text-white bg-cyan rounded-full hover:opacity-70">Logout</a>
+          <>
+            <a 
+              href="#" 
+              className="px-8 py-3 font-bold text-white bg-cyan rounded-full hover:opacity-70" 
+              onClick={logoutHandler}>Logout
+            </a>
+          </>
         )
       }
     </>
