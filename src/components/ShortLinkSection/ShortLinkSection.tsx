4603361976c44b1412c0040cb0daf3b0c9a9810a
Nick <nsagias@gmail.com>
2023-04-22 23:16:29 -0400

    pass shortlink section

--- src/components/ShortLinkSection/ShortLinkSection.tsx
+++ src/components/ShortLinkSection/ShortLinkSection.tsx
@@ -1,13 +1,13 @@
 import ShortLinkForm from "./ShortLinkForm";
 import ShortLinkContainer from "./ShortLinkSectionContainer";
   
-export default function ShortLinkSection() {
+export default function ShortLinkSection({ shortLinkData }: { shortLinkData: any }):JSX.Element  {
   return (
     <section id="shorten-section" className="relative bg-gray-100 pb-10">
       {/* Shorten Section Container */}
       <ShortLinkContainer>
         {/* Shorten Form */}
-        <ShortLinkForm />
+        <ShortLinkForm shortLinkData={shortLinkData}/>
       </ShortLinkContainer>
     </section>
   );
