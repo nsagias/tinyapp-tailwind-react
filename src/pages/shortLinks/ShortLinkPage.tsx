12beeb9695f71973b20d9de452057653c5bbfac4
Nick <nsagias@gmail.com>
2023-04-22 23:24:08 -0400

    add get params, updaet to get token and user info, get data from backend, pass data down to components

--- src/pages/shortLinks/ShortLinkPage.tsx
+++ src/pages/shortLinks/ShortLinkPage.tsx
@@ -1,11 +1,39 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import localStorageService from "../../services/localStorageService";
 import HeroShortLinksSection from "../../components/heroSection/HeroShortLinksSection";
 import ShortLinkSection from "../../components/shortLinkSection/ShortLinkSection";
+import { getLinkByUserIdAndShortLink } from "../../api/linkApi";
+import { LinkSuccessReponse } from "../../types/api/linkApi";
 
 export default function ShortLinkPage({}): JSX.Element {
+  const selectedShortLink = useParams();
+  const [shortLinkData, setShortLinkData] = useState<LinkSuccessReponse>();
+  const [userId, setUserId] = useState<number>(JSON.parse(localStorageService.getLocalStorageItem("id")!));
+  const [shortUrl, setShortUrl] = useState<any>(selectedShortLink);
+  const [token, setToken] = useState(localStorageService.getLocalStorageItem("token"));
+
+  useEffect(() => {
+    if (userId && token) {
+      getSelectedShortLink({userId, shortUrl}, token);
+    }
+  }, []);
+
+  const getSelectedShortLink = async (requesInfo: any, token: string) => {
+    try {
+      const data = await getLinkByUserIdAndShortLink(requesInfo, token);
+      if (!data) throw new Error("no data");
+      setShortLinkData(data);
+
+    } catch (error: any) {
+      console.log("Error", error)
+    }
+  };
+
   return (
     <>
-      <HeroShortLinksSection />
-      <ShortLinkSection />
+      <HeroShortLinksSection shortLinkData={shortLinkData}/>
+      <ShortLinkSection shortLinkData={shortLinkData}/>
     </>
   );
 }
