345e576e356dc2f56ec164674992b2ae8e658f93
Nick <nsagias@gmail.com>
2023-04-24 22:52:54 -0400

    update types for onShortLinkData

--- src/components/ShortLinkSection/ShortLinkSection.tsx
+++ src/components/ShortLinkSection/ShortLinkSection.tsx
@@ -1,7 +1,8 @@
+import { UpdateShortLinkResponse } from "../../types/api/linkApi";
 import ShortLinkForm from "./ShortLinkForm";
 import ShortLinkContainer from "./ShortLinkSectionContainer";
   
-export default function ShortLinkSection({ onShortLinkData, onShortLinkDataChanged }: { onShortLinkData: any, onShortLinkDataChanged: any }):JSX.Element  {
+export default function ShortLinkSection({ onShortLinkData, onShortLinkDataChanged }: { onShortLinkData: UpdateShortLinkResponse, onShortLinkDataChanged: (shortLinkData: UpdateShortLinkResponse) => void }):JSX.Element  {
   return (
     <section id="shorten-section" className="relative bg-gray-100 pb-10">
       {/* Shorten Section Container */}
