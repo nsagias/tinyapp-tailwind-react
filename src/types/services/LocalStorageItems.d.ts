2852bf325f5a275d9255a22b7e8c3d0a3f6721ab
Nick <nsagias@gmail.com>
2023-04-24 21:26:24 -0400

    create enum

---
+++ src/types/services/LocalStorageItems.d.ts
@@ -0,0 +1,10 @@
+enum LocalStorageItem {
+  Active = "active",
+  Email = "email",
+  EmailVerified = "emailVerified",
+  FirstName = "firstName",
+  LastName = "lastName",
+  Id = "id",
+  Token = "token",
+  IsAuthenticated = "isAuthenticated"
+}
