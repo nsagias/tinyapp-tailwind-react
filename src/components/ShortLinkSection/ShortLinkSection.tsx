4f5cb59d233df080499fff9a897bc435910f839f
Nick <nsagias@gmail.com>
2023-04-24 20:37:58 -0400

    pass down shortLinkData state and setShortLinkData

--- src/components/ShortLinkSection/ShortLinkSection.tsx
+++ src/components/ShortLinkSection/ShortLinkSection.tsx
@@ -1,13 +1,16 @@
 import ShortLinkForm from "./ShortLinkForm";
 import ShortLinkContainer from "./ShortLinkSectionContainer";
   
-export default function ShortLinkSection({ shortLinkData }: { shortLinkData: any }):JSX.Element  {
+export default function ShortLinkSection({ onShortLinkData, onShortLinkDataChanged }: { onShortLinkData: any, onShortLinkDataChanged: any }):JSX.Element  {
   return (
     <section id="shorten-section" className="relative bg-gray-100 pb-10">
       {/* Shorten Section Container */}
       <ShortLinkContainer>
         {/* Shorten Form */}
-        <ShortLinkForm shortLinkData={shortLinkData}/>
+        <ShortLinkForm
+          onShortLinkData={onShortLinkData}
+          onShortLinkDataChanged={onShortLinkDataChanged}
+        />
       </ShortLinkContainer>
     </section>
   );
