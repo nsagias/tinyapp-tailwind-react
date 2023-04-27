060e9c22af66eeb8554d0d32543e49b47db886f5
Nick <nsagias@gmail.com>
2023-04-26 21:11:56 -0400

    fix path

--- src/components/navBar/NavLogo.tsx
+++ src/components/navBar/NavLogo.tsx
@@ -6,7 +6,7 @@ export default function NavLogo() {
       <button>
         <Link to={"/"}>
           <img
-            src="images/logo-tiny-app.svg"
+            src="/images/logo-tiny-app.svg"
             alt=""
             className="w-32 border-8 border-cyan px-4 py-1 pt-2 rounded-full hover:opacity-70"
           />
