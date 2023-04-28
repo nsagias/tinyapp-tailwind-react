4187c39e8b33b0d6c11a3ae0cea582fe86e27ee7
Nick <nsagias@gmail.com>
2023-04-28 16:53:08 -0400

    pass down mobile menu state

--- src/components/navBar/NavButtonHamburger.tsx
+++ src/components/navBar/NavButtonHamburger.tsx
@@ -1,16 +1,17 @@
+import React from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
-export default function NavButtonHamburger({isOpen}: {isOpen: boolean}): JSX.Element {
+export default function NavButtonHamburger({onOpen, onSetOpen}: {onOpen: boolean, onSetOpen: (isOpen: boolean) => any }): JSX.Element {
   return (
     <>
-      {isOpen ? 
+      {onOpen ? 
         (    
-          <button id="menu-button" type="button" className="block text-2xl lg:hidden focus:outline-none hover:text-grayViolet">
+          <button id="menu-button" onClick={() => onSetOpen(false)} type="button" className="block text-2xl lg:hidden focus:outline-none hover:text-grayViolet">
             <FaTimes />
           </button>
         ) : 
         (    
-          <button id="menu-button" type="button" className="block text-2xl lg:hidden focus:outline-none hover:text-grayViolet">
+          <button id="menu-button" onClick={() => onSetOpen(true)} type="button" className="block text-2xl lg:hidden focus:outline-none hover:text-grayViolet">
             <FaBars />
           </button>
       )}
