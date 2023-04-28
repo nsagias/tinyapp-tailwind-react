6e78e55a24c2fe2e85adbb7e502d8c5b2f9390d0
Nick <nsagias@gmail.com>
2023-04-28 13:33:14 -0400

    create constants to conditonally filter based on login and role level

--- src/App.tsx
+++ src/App.tsx
@@ -20,30 +20,36 @@ export default function App() {
 
   const selectIsAuthenticated = useSelector((state: RootState) => state.authentication.isAuthenticated);
   const [isAthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const isLoggedIn = isAthenticated ? 1 : 0;
+  const rolelevel = 3;
 
   useEffect( () => {
     setIsAuthenticated(selectIsAuthenticated);
   }, [selectIsAuthenticated ]);
-  // const routesAuthList = [
-  //   {path:"/features", element:[<FeaturesPage />], logginRequired: isLoggedIn},
-  //   {path:"/pricing", element:[<PricingPage />],logginRequired: isLoggedIn},
-  //   {path:"/docs", element:[<DocsPage />], logginRequired: isLoggedIn},
-  //   {path:"/login", element:[<LoginPage />], logginRequired: isLoggedIn},
-  //   {path:"/register", element:[<RegistrationPage />],logginRequired: isLoggedIn},
-  //   {path:"/shorturls", element:[<ShortenUrlPage />], logginRequired: isLoggedIn},
-  //   {path:"/shorturls/:id", element:[<ShortLinkPage />], logginRequired: isLoggedIn},
-  //   {path:"/", element:[<HomePage />], ilogginRequired: isLoggedIn }, 
-  //   {path:"*", element:[<NotFoundPage />], logginRequired: isLoggedIn},
-  // ];
-  // const routeAuth =  routesAuthList && Array.isArray(routesAuthList) && routesAuthList.map((route, index) => (
-  //   <Route path={`${route.path}`} element={route.element} key={index} />
-  // ));
+
+  const routesAuthList = [
+    {path:"/features",  element:[<FeaturesPage />],     logginRequired: 0, role: 4},
+    {path:"/pricing",   element:[<PricingPage />],      logginRequired: 0, role: 4},
+    {path:"/docs",      element:[<DocsPage />],         logginRequired: 0, role: 4},
+    {path:"/login",     element:[<LoginPage />],        logginRequired: 0, role: 4},
+    {path:"/register",  element:[<RegistrationPage />], logginRequired: 0, role: 4},
+    {path:"/shorturls", element:[<ShortenUrlPage isAuthenticated={isAthenticated} />], logginRequired: 1, role: 3},
+    {path:"/shorturls/:id", element:[<ShortLinkPage />],logginRequired: 1, role: 3},
+    {path:"/",          element:[<HomePage />],         logginRequired: 0, role: 4},
+    {path:"*",          element:[<NotFoundPage />],     logginRequired: 0, role: 4},
+  ];
+
+  const routeAuth = routesAuthList && Array.isArray(routesAuthList) && routesAuthList.filter(x => x.logginRequired <= isLoggedIn).filter(x => x.role >= rolelevel).map((route, index) => (
+    <Route path={`${route.path}`} element={route.element} key={index} />
+  ));
+  console.log("ROUTE AUTH", routeAuth);
+
 
   return (
     <div className="App">
-      <NavBar authorized={isAthenticated} />
+      <NavBar isAthenticated={isAthenticated} />
         <Routes>
-          <Route path="/features" element={<FeaturesPage />} />
+          {/* <Route path="/features" element={<FeaturesPage />} />
           <Route path="/pricing" element={<PricingPage />} />
           <Route path="/docs" element={ <DocsPage />} />
           <Route path="/login" element={<LoginPage />} />
@@ -51,7 +57,9 @@ export default function App() {
           <Route path="/shorturls" element={<ShortenUrlPage isAuthenticated={isAthenticated} /> } />
           <Route path="/shorturls/:id" element={<ShortLinkPage />}/>
           <Route path="/" element={<HomePage />} /> 
-          <Route path="*" element={<NotFoundPage />} />
+          <Route path="*" element={<NotFoundPage />} /> */}
+
+          {routeAuth}
         </Routes>
       <Footer authorized={true}/>
     </div>
