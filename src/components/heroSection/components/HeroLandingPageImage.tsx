ed0ad1d63cc4c7e6a20cb6a084d6e82073e45fc8
Nick <nsagias@gmail.com>
2023-04-28 13:41:41 -0400

    add igunna image to landing page

--- src/components/heroSection/components/HeroLandingPageImage.tsx
+++ src/components/heroSection/components/HeroLandingPageImage.tsx
@@ -1,7 +1,7 @@
 export default function HeroImage() {
   return (
     <div className="mb-24 mx-auto md:w-180 lg:mb-0 lg:w-1/2">
-      <img src="/images/illustration-working.svg" alt="" className="" />
+      <img src="/images/iguana-landing-page.png" alt="" />
     </div>
   );
 }
