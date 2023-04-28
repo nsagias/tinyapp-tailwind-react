d0c0509d3ab76bf1e69902ca168b5a5b7cc1b9f6
Nick <nsagias@gmail.com>
2023-04-27 23:25:18 -0400

    add prop onSetCreateShortLinkResponse, remove unused imports, remove console.logs

--- src/components/shortenSection/ShortenForm.tsx
+++ src/components/shortenSection/ShortenForm.tsx
@@ -1,46 +1,32 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { validURL } from "../Utils/utils";
 import { createShortLink } from "../../api/linkApi";
 import localStorageService from "../../services/localStorageService";
 
-export default function ShortenForm() {
+export default function ShortenForm({ onSetCreateShortLinkResponse }: { onSetCreateShortLinkResponse: any }): JSX.Element {
 
   const [longUrl, setLongUrl] = useState<string>("");
   const [errorMessage, setErrorMessage] = useState<string | undefined>("");
   const [userId, setUserId] = useState<number>(JSON.parse(localStorageService.getLocalStorageItem("id")!));
   const [token, setToken] = useState(localStorageService.getLocalStorageItem("token"));
 
-  useEffect(() => {
-
-  }, []);
   
   const handleSubmitLink = async (e: React.FormEvent): Promise<void> => {
     e.preventDefault();
     
     // check for value
-    if (longUrl === "") {
-      return setErrorMessage("Please Enter Value");
-    }
- 
+    if (longUrl === "") return setErrorMessage("Please Enter Value");
+    
     // check for valid link
     const isValidURL = validURL(longUrl);
-    if (!isValidURL) {
-      return setErrorMessage("Please Valid Link");
-    }
-   
+    if (!isValidURL) return setErrorMessage("Please Valid Link");
 
-   try {
-  
-    // userId: number;
-    // longUrl: string;
-    console.log()
+    try {
       const createShortLinkResponse = await createShortLink({userId, longUrl}, token!)
-      console.log("CREATE SHORT LINK RESPOSNE", createShortLinkResponse);
-
-   } catch (error: any) {
-    console.log(error)
-    
-   }
+      onSetCreateShortLinkResponse(createShortLinkResponse);
+    } catch (error: any) {
+      console.log(error)
+    }
     // clear error message
     setErrorMessage("");
     setLongUrl("");
@@ -49,7 +35,9 @@ export default function ShortenForm() {
   return (
     <form onSubmit={handleSubmitLink} 
       id="link-form" 
-      className="relative flex flex-col w-full p-10 -mt-20 space-y-4 bg-darkViolet rounded-lg md:flex-row md:space-y-0 md:space-x-3">
+      className="relative flex flex-col w-full p-10 -mt-20 space-y-4 bg-darkViolet rounded-lg md:flex-row md:space-y-0 md:space-x-3"
+    >
+
       <input
         className={`flex-1 p-3 border-2 rounded-lg placeholder-yellow-500 focus:outline-none ${errorMessage && "border-red border-8"}`}
         id="shorten-link" 
@@ -58,12 +46,14 @@ export default function ShortenForm() {
         placeholder={"Enter Link Here"}
         value={longUrl}
         onChange={(e) => setLongUrl(e.target.value)}
-        >  
+      >
       </input>
+
       <button
         className="px-10 py-3 text-white bg-cyan rounded-lg hover:bg-cyanLight focus:outline-none md:py-2" 
         disabled={false}
-        >Shorten Link</button>
+        >Shorten Link
+      </button>
         
       {/* Error Message */}
       { errorMessage && (
