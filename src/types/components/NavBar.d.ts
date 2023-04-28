87d4b1d9fbbd0ad210c1dbb4651e891a1ea18d92
Nick <nsagias@gmail.com>
2023-04-28 13:32:05 -0400

    update types for login and role

--- src/types/components/NavBar.d.ts
+++ src/types/components/NavBar.d.ts
@@ -2,17 +2,23 @@
 export type NavBarMenuItemName = { 
   menuItemName: string, 
   url: string,
+  logginRequired?: number,
+  role?: number,
 }; 
 
 export type NavBarMenuItemData = {
   id: number,
   menuItemName: string,
   url: string,
-  isLoggedIn?: boolean
+  isLoggedIn?: boolean,
+  logginRequired: number,
+  role?: number,
 };
 
 export type MobileMenuItem = {
   linkURL: string,
   styling: string,
   buttonName: string,
+  logginRequired?: number,
+  role?: number,
 };
