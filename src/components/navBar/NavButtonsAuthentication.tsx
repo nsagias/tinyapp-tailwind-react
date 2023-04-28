d40e7fa6216396f13b565795e3b5fda3d521049c
Nick <nsagias@gmail.com>
2023-04-28 16:34:39 -0400

    move logout handler up one level

--- src/components/navBar/NavButtonsAuthentication.tsx
+++ src/components/navBar/NavButtonsAuthentication.tsx
@@ -1,30 +1,22 @@
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { setAuthFalse } from "../../store/slices/authenticationSlice";
+import { Link } from "react-router-dom";
 
-export default function NavButtonsAuthentication({ authorized }: { authorized : boolean}): JSX.Element {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
 
-  const logoutHandler = () => {  
-    dispatch(setAuthFalse());
-    navigate("/");
-  };
+export default function NavButtonsAuthentication({ isAthenticated, onLogout }: { isAthenticated: boolean,  onLogout: () => void; }): JSX.Element {
 
   return (
     <>
-      {!authorized ? 
+      {!isAthenticated ? 
         (
           <>
             <Link to={"/login"} className="hover:text-darkerViolet">Login</Link>
-            <Link to={"/register"} className="px-8 py-3 font-bold text-white bg-cyan rounded-full hover:opacity-70" >Sign Up</Link>
+            <Link to={"/register"} className="px-8 py-3 font-bold text-white bg-cyan rounded-full hover:opacity-70">Sign Up</Link>
             </>      
         ) : (
           <>
             <a 
               href="#" 
               className="px-8 py-3 font-bold text-white bg-cyan rounded-full hover:opacity-70" 
-              onClick={logoutHandler}>Logout
+              onClick={onLogout}>Logout
             </a>
           </>
         )
