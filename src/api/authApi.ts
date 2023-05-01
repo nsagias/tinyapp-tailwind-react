4151961367087b7fc495bf26933d030654f61076
Nick <nsagias@gmail.com>
2023-04-30 21:14:09 -0400

    add path for axios

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,3 +1,5 @@
 import axios, { AxiosInstance } from "axios";
 
-const authApi: AxiosInstance = axios.create();
+const authApi: AxiosInstance = axios.create({
+  baseURL: 'http://0.0.0.0:3001/'
+});
