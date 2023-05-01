3dce394bb9527616fcdc03e4e7e456328308777f
Nick <nsagias@gmail.com>
2023-04-30 21:16:02 -0400

    add clipboard to function

--- src/components/buttons/CopyToClipboardButton.tsx
+++ src/components/buttons/CopyToClipboardButton.tsx
@@ -2,9 +2,14 @@ import { CopyToClipboardButtonProps } from "../../types/components/Buttons";
 import { copyToClipboard } from "../Utils/utils";
 
 export default function CopyToClipboardButton({ copy, className} : CopyToClipboardButtonProps): JSX.Element {
+  
+  const handleCopy = async () => {
+    await copyToClipboard(copy);
+  };
+
   return (
     <button  
-      onClick={() =>  copyToClipboard(copy)} 
+      onClick={() =>  handleCopy()}
       className={className}>Copy
     </button>
   );
